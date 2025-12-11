de5d974e29b3e451fb0e1bd457ad87c4a7129a31
Bryan Smith <brabsmit@gmail.com>
2025-12-11 12:21:40 -0500

    Initial Commit

---
+++ Permute/PermuteApp.swift
@@ -0,0 +1,17 @@
+//
+//  PermuteApp.swift
+//  Permute
+//
+//  Created by Bryan Smith on 12/11/25.
+//
+
+import SwiftUI
+
+@main
+struct PermuteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PermuteTests/PermuteTests.swift
@@ -0,0 +1,17 @@
+//
+//  PermuteTests.swift
+//  PermuteTests
+//
+//  Created by Bryan Smith on 12/11/25.
+//
+
+import Testing
+@testable import Permute
+
+struct PermuteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
